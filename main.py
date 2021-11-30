77bec6a79f753c37f81305a265988721b222693b
karinaensh <92182063+karinaensh@users.noreply.github.com>
2021-11-30 12:02:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def kbig(nums, k):
+    max_num=max(nums)
+    for i in range(k):
+        n=max(nums)
+        nums.remove(int(n))
+    print(n)
+    
